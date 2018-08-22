aa83a2b695fd22311b85de40aa3c306fdee70d8e
chenjie <jery1997@foxmail.com>
2018-08-22 14:32:33 +0800

    Update element.js

--- element.js
+++ element.js
@@ -4,13 +4,42 @@ this.type=type;
 this.props=props;
 this.children=children;
 }
+ static setAttr(node,key,value){
+    let tagName=node.tagName;
+    switch (key) {
+        case "value":
+        if(tagName.toUpperCase()==="BUTTON"||tagName.toUpperCase()==="INPUT"||tagName.toUpperCase()==="TEXTAREA"){
+           node.value=value;
+        }else{
+            node.setAttribute(key,value);
+        }
+        break;
+        case 'style':
+node.style.cssText=value;
+            break;
+    case 'className':
+    node.setAttribute('class',value)
+        default:
+        node.setAttribute(key,value)
+            break;
+    }
 }
-function createElement(type,props,children){
-    return new Element(type,props,children)
 }
-function render(eleObj){
-let el=document.createElement(eleObj.type);
-for (let key in eleObj.props){
-    setAttr(el,key,eleObj.props[key])
+
+let render=(vistualDom)=>{//vNode转换为真实Dom
+let el=document.createElement(vistualDom.type);
+for (let key in vistualDom.props){
+Element. setAttr(el,key,vistualDom.props[key]);
 }
+vistualDom.children.forEach(element => {
+   let child=(element instanceof Element)?render(element):document.createTextNode(element);
+    el.appendChild(child)
+});
+return el;
+}
+let renderDom=(el,target)=>{
+document.querySelector(target).appendChild(el);
+}
+let createElement=(type,props,children)=>{
+   return new Element(type,props,children)
 }
